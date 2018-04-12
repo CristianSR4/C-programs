b837f96ee85fd59619087a1404a5093cbc471be0
CristianSR4 <cristian.lerida.sanchez@gmail.com>
2018-04-12 13:55:20 +0200

    for

---
+++ Repaso/Ejercicio27.cpp
@@ -0,0 +1,14 @@
+/* Muestra los numero del 15 al 5 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int numero;
+
+    for (numero=15; numero>=5; numero--)
+        printf("%i \n", numero);
+
+  return EXIT_SUCCESS;
+}
