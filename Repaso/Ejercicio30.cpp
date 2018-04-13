e5cd69fa220f6017de5013e7c2919abc50d23b5b
CristianSR4 <cristian.lerida.sanchez@gmail.com>
2018-04-13 14:21:00 +0200

    Tabla de multiplicar del 5

---
+++ Repaso/Ejercicio30.cpp
@@ -0,0 +1,18 @@
+/* Tabla de multiplicar del 5.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int numero;
+    int tabla;
+
+    for(tabla=5; tabla<=5; tabla++) {
+        for(numero=1; numero<=10; numero++)
+            printf("La tabla de multiplicar %i por %i es: %i \n", tabla, numero, tabla*numero);
+      }
+
+
+  return EXIT_SUCCESS;
+}
