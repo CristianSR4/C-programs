23a7e564153199caa5856cae49b696596e992788
CristianSR4 <cristian.lerida.sanchez@gmail.com>
2018-04-26 19:17:49 +0200

    Estructuras terminado

--- Repaso/ejercicio41.cpp
+++ Repaso/ejercicio41.cpp
@@ -1,3 +1,4 @@
+/* Programa pregunta datos de una cancion y los muestra en pantalla*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,7 +16,22 @@ int main(){
   fgets(cancion.artista, 15, stdin);
   fflush(stdin);
 
-  printf("El nombre del artista es: %s\n", cancion.artista);
+  printf("Escribe el titulo de la cancion: ");
+  fgets(cancion.titulo, 15, stdin);
+  fflush(stdin);
+
+  printf("Escribe la duracion de la cancion: ");
+  scanf("%i", &cancion.duracion);
+  
+  printf("Escribe el tamaño de la cancion: ");
+  scanf("%i", &cancion.tamano);
+
+  printf("\n");
+
+  printf("El nombre del artista es: %s", cancion.artista);
+  printf("El titulo de la cancion es: %s", cancion.titulo);
+  printf("La duracion de la cancion es: %i\n", cancion.duracion);
+  printf("El tamano de la cancion es: %i\n", cancion.tamano);
 
   return EXIT_SUCCESS;
 }
