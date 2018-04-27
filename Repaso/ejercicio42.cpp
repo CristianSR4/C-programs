751bfe57542f6206527354324b48ff8a898ceff0
CristianSR4 <cristian.lerida.sanchez@gmail.com>
2018-04-27 14:27:15 +0200

    Por terminar

---
+++ Repaso/ejercicio42.cpp
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define gente 50
+
+int main(){
+
+    struct {
+        char nombre[15];
+        char direccion[30];
+        int telefono;
+        int edad;
+    } persona;
+
+    int l;
+    int opcion;
+
+    for(l=0; l<gente; l++) {
+    printf("Escribe nombre: ");
+    fgets(persona.nombre, 15 , stdin);
+    fflush(stdin);
+
+    printf("Escribe su direccion: ");
+    fgets(persona.direccion, 30 , stdin);
+    fflush(stdin);
+
+    printf("Escribe su numero de telefono: ");
+    scanf("%i", &persona.telefono);
+
+    printf("Escribe su edad: ");
+    scanf("%i", &persona.edad);
+
+   // while(scanf("%[^\n]", persona.nombre[l++]));
+    }
+
+    printf("1.Mostrar la lista de todos los nombre.\n");
+    printf("2.Mostrar las personas de una cierta edad.\n");
+    printf("3.Mostrar las personas cuya inicial sea la que el usuario indique\n");
+    printf("4.Salir del programa\n");
+
+  return EXIT_SUCCESS;
+}
